654dd322215fae8223a5ed71e1476d639335798a
Alok <boyeeaalok@gmail.com>
2026-01-15 15:49:33 -0600

    Using langchain, finished the chunking step

--- index.py
+++ index.py
@@ -1,8 +1,17 @@
+from langchain_community.document_loaders import PyPDFLoader
+
 from pathlib import Path
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 pdf_path = Path(__file__).parent / "plato.pdf"
 
-from langchain_community.document_loaders import PyPDFLoader
 #load this file in python program
 loader = PyPDFLoader(pdf_path)
-
 docs= loader.load()
+
+#spilt the docs into smaller chunks
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=400 
+        
+)
+chunks= text_splitter.split_documents(documents=docs)
