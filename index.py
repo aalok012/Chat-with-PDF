448ee7eeed863bc772e422db2e8e9b4e54edd637
Alok <boyeeaalok@gmail.com>
2026-01-15 16:09:26 -0600

    Done with the creation and storage of vector db and started the retrieval part

--- index.py
+++ index.py
@@ -1,8 +1,12 @@
+from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
-
 from pathlib import Path
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
 pdf_path = Path(__file__).parent / "plato.pdf"
+
+load_dotenv()
 
 #load this file in python program
 loader = PyPDFLoader(pdf_path)
@@ -15,3 +19,23 @@ text_splitter = RecursiveCharacterTextSplitter(
         
 )
 chunks= text_splitter.split_documents(documents=docs)
+
+#next step is to create vector embeddings from the chunk
+
+#vector embeddings
+embedding_model = OpenAIEmbeddings(
+    model="text-embedding-3-large"
+)
+
+#now the vector embedding created above for the chunks and should be stored in the qdrant db
+
+vector_store = QdrantVectorStore.from_documents(
+    documents= chunks,
+    embedding = embedding_model,
+    url = "http://localhost:6333", 
+    collection_name ="RAG"
+    
+    
+)
+print("Indexing of documents")
+ 
